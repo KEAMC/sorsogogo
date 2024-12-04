93baea33f8a5407791f037c9db4e0378674c54f1
Vince Cedie Pino <vincecediepino@gmail.com>
2024-12-04 13:55:03 +0700

    Add colors to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
-      }
+      },
+      colors: {
+        "lincoln-green": "#195905",
+        "dark-green": "#133E04",
+        "golden-yellow": "#FFD700",
+      },
     },
   },
   plugins: [],
